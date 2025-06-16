7123d3c69aaad2e0190e35f3518955c7a401d31a
Radlant <radiantnonton@gmail.com>
2025-06-16 12:44:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function changeColor() {
   const randomColor = `hsl(${Math.floor(Math.random() * 360)}, 70%, 80%)`;
   document.body.style.backgroundColor = randomColor;
+  const sound = document.getElementById("clickSound");
+sound.currentTime = 0; // rewind to start if it's already playing
+sound.play();
 }
